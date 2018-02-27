0823499854dbcbe9770ea41b3199f9d85e94e599
Abhimanyue Singh Tanwar <abc@example.com>
2018-02-28 01:25:14 +0800

    Fix login bug, added session middleware

--- routes/login.js
+++ routes/login.js
@@ -2,20 +2,25 @@ var express = require('express');
 var router = express.Router();
 var Account= require('../models/account');
 
+
 router.get('/',function(req,res){
   res.render('login', { title: "login" });
 });
 
 
 router.post('/',function(req,res){
-  if (!req.body.emailId || !req.body.password){
+  if (!req.body.fullName || !req.body.password){
+
     return res.render('login', { title: "login" , message: "Please Enter both username and password"});
   }
   //finding username from account database
-  Account.findOne({emailId:req.body.emailId},function(error,account)
-  {
-    if (error) return console.log("error in accessing the database");
-    else if (!account) return res.render('login', { title: "login" , message: "emailId doesnot Exists"});
+  Account.findOne({fullName: JSON.stringify(req.body.fullName)},function(error,account)
+  { 
+    if (error) 
+      return console.log("Error aaya");
+
+    if (!account)
+      return console.log(account); //return res.render('login', { title: "login" , message: "emailId doesnot Exists"});
     // creating a new session
 
     if (account.compare(req.body.password)){
