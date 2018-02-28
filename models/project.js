227e77594e20017fe604b3b239dfd86872f8f2a8
Sudhakar Shah <sudhakarshah2013@gmail.com>
2018-03-01 00:34:17 +0800

    Adding Tags to database

--- models/project.js
+++ models/project.js
@@ -2,10 +2,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var projectSchema = new Schema({
+  author: Schema.Types.ObjectId,
   title: {type:String, required: true},
   description:{type:String, required:true},
   //image:{ data: Buffer, contentType: String }
-  imagePath:{type:String,required:true}
+  imagePath:{type:String,required:true},
+  tags: [Schema.Types.ObjectId],
+  roles:[Schema.Types.ObjectId]
 
 })
 
--- routes/newproject.js
+++ routes/newproject.js
@@ -2,11 +2,47 @@ var express = require('express');
 var router = express.Router();
 var multer = require('multer');
 var project= require('../models/project');
+var Tags=require('../models/tag');
 
 // get function to add a new image
 router.get('/', function(req, res, next) {
  res.render('addproject');
 });
+
+
+var authenticate = function(req,res,next){
+  if (req.session && req.session.user) return next();
+  return res.redirect('/login');
+}
+
+// creating a list of tag id for the project. Adding new tags to the database if doesnot exist
+var tagHandler =function(list){
+  var listOfTagsId=[];
+  for(var string of list){
+    Tags.findOne({name: string},function(error,tag){
+      if (error)
+        return console.log("Error in accessing database");
+      if (!tag)
+      {
+        Tags.create({
+          name: string
+        },function(error,addedTag){
+          if(error) return console.log("Error in adding tags to database");
+          console.log("Tags created");
+          listOfTagsId.push(addedTag._id);}
+        );
+      }
+      else {
+        listOfTagsId.push(tag._id);
+      }
+    });
+  }
+  return listOfTagsId;
+
+}
+
+
+
 
 // storing the project image in uploads folder and setting the path
 var storage = multer.diskStorage({
@@ -19,7 +55,7 @@ var storage = multer.diskStorage({
 })
 var upload = multer({ storage: storage });
 
-router.post('/', upload.any(), function(req, res, next) {
+router.post('/',authenticate,upload.any(), function(req, res, next) {
  //res.send(req.files);
  // path of the project images
  var path = req.files[0].path;
@@ -30,15 +66,22 @@ var imageName = req.files[0].originalname;
  imagepath['path'] = path;
  imagepath['originalname'] = imageName;
 */
+listOfTagsId=tagHandler(listoftags send by frontend)
+listOfRolesId=roleHandler(listofRoles send by frontend)
+
+
 // Adding the new image in the database
  project.create({
+   author:req.session.user._id,
    title: req.body.title,
    description: req.body.description,
-   imagePath: path
-   },function(error,post){
-     if(error) return console.log("Error in project to database");
+   imagePath: path,
+   tags:listOfTagsId,
+   roles:listOfRolesId
+ },function(error,addedProject){
+     if(error) return console.log("Error in adding project to database");
      console.log("Project created");
-     res.send(post);
+     res.send(addedProject);
    }
  );
 
