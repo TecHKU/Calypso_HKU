838be01cd51d6a54fbf8057c00ea8c4c07025b55
Sudhakar Shah <sudhakarshah2013@gmail.com>
2018-03-12 23:54:17 +0800

    integrated react and backend

--- app.js
+++ app.js
@@ -20,9 +20,11 @@ var db = mongoose.connect('mongodb://localhost:27017/calpy');
 var app = express();
 
 // view engine setup
+/*
 app.engine('pug', require('pug').__express);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+*/
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -30,12 +32,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'build')));
 app.use(session({secret: 'ssshhhhh',
 resave: false,
     saveUninitialized: false}));
 
-app.use('/', index);
+app.use('/', express.static(path.join(__dirname, 'build'))); //app.use('/', index);
 app.use('/users', users);
 app.use('/newproject',newproject);  // handling creation of new project
 app.use('/login', login);
