e27023829d32c7312fa10f48b1a9627cba331b61
Sudhakar Shah <sudhakarshah2013@gmail.com>
2018-03-14 00:12:01 +0800

    commented engine

--- app.js
+++ app.js
@@ -24,9 +24,11 @@ var app = express();
 
 // view engine setup
 
+/*
 app.engine('pug', require('pug').__express);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+*/
 
 
 // uncomment after placing your favicon in /public
