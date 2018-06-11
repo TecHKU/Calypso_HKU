09e93c393820ab858f5e6d64fb2f893aa220a6d5
Sudhakar Shah <sudhakarshah2013@gmail.com>
2018-06-12 00:51:15 +0800

    spacing

--- routes/verify.js
+++ routes/verify.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var Account= require('../models/account');
+var Account = require('../models/account');
 
 let standardResponse = {
     success: false,
@@ -8,23 +8,22 @@ let standardResponse = {
 };
 
 router.get('/',function(req,res){
-
-  var host=req.get('host');
-  console.log(req.protocol+":/"+req.get('host'));
-  if((req.protocol+"://"+req.get('host'))==("http://"+host))
+  var host = req.get('host');
+  console.log(req.protocol + ":/" + req.get('host'));
+  if((req.protocol + "://" + req.get('host')) == ("http://" + host))
   {
       console.log("Domain is matched. Information is from Authentic email");
-      Account.findOne({verificationLink:req.query.id},function(error,account){
+      Account.findOne({verificationLink : req.query.id},function(error, account){
         if(error){
-          standardResponse.reason="error in accessing account database while findinf account";
+          standardResponse.reason = "error in accessing account database while findinf account";
           return res.send(standardResponse);
         }
         if(!account){
           standardResponse.reason = "No account for this link exists";
           return res.send(standardResponse);
         }
-        account.isVerified=true;
-        account.verificationLink="";
+        account.isVerified = true;
+        account.verificationLink = "";
         account.save(function(error){
           if(error){
             standardResponse.reason = "error while accessing database to update verification status";
@@ -36,11 +35,10 @@ router.get('/',function(req,res){
           }
         })
       })
-
   }
   else
   {
-      standardResponse.reason="Bad Request wrong protocol";
+      standardResponse.reason = "Bad Request wrong protocol";
       return res.send(standardResponse);
   }
 });
