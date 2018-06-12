57771ec1711c3aa87be4281cff4e511e1d3cfa06
Sudhakar Shah <sudhakarshah2013@gmail.com>
2018-06-12 18:24:28 +0800

    bugs removed and logic changed

--- routes/verify.js
+++ routes/verify.js
@@ -13,32 +13,22 @@ router.get('/',function(req,res){
   if((req.protocol + "://" + req.get('host')) == ("http://" + host))
   {
       console.log("Domain is matched. Information is from Authentic email");
-      Account.findOne({verificationLink : req.query.id},function(error, account){
-        if(error){
-          standardResponse.reason = "error in accessing account database while findinf account";
-          return res.send(standardResponse);
-        }
-        if(!account){
-          standardResponse.reason = "No account for this link exists";
-          return res.send(standardResponse);
-        }
-        account.isVerified = true;
-        account.verificationLink = "";
-        account.save(function(error){
-          if(error){
-            standardResponse.reason = "error while accessing database to update verification status";
+      Account.findOneAndUpdate({verificationLink : req.query.id},{isVerified : true, verificationLink : "" },function(error,account){
+          if (error) {
+            standardResponse.reason = "error in accessing account database " + error;
+            return res.send(standardResponse);
+          } 
+          if (!account) {
+            standardResponse.reason = "No account for this link exists";
             return res.send(standardResponse);
           }
-          else {
-            standardResponse.success = true;
-            return res.send(standardResponse);
-          }
-        })
+          standardResponse.success = true;
+          return res.send(standardResponse);
       })
   }
   else
   {
-      standardResponse.reason = "Bad Request wrong protocol";
+      standardResponse.reason = "Bad request wrong protocol";
       return res.send(standardResponse);
   }
 });
