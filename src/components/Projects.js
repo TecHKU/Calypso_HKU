955fcb8c9c91e0cb537cdb2fa5381ca07df310d8
utkarsh867 <utkarsh867@gmail.com>
2018-06-12 13:26:23 +0530

    search projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SearchBar from "./SearchBar";
 import ProjectsScreen from "./ProjectsScreen";
+import axios from "axios/index";
 
 /**
  * @author utkarsh867
@@ -8,18 +9,50 @@ import ProjectsScreen from "./ProjectsScreen";
  */
 class Projects extends Component {
 
+    state = {
+        projects: [],
+        loading: true,
+        displayProjects: []
+    };
+
+
+    componentWillMount(){
+        this.fetchProjects().then((projects) =>{
+            this.setState({
+                projects: projects,
+                displayProjects: projects,
+                loading: false
+            })
+        });
+    }
+
+    fetchProjects = async() =>{
+        const response = await axios.get('/api/projects', {withCredentials: true});
+        return response.data;
+    };
+
     onSearch = (val) => {
-        console.log(val);
+        const displayProjects = [];
+        for(let i = 0; i<this.state.projects.length; i++){
+            if(((this.state.projects[i].title).toLowerCase()).indexOf((val.toLowerCase())) >= 0){
+                displayProjects.push(this.state.projects[i]);
+            }
+        }
+
+        this.setState({
+            displayProjects: displayProjects
+        });
+
     };
 
     render() {
         return (
             <div className="col-lg-12 projects">
                 <SearchBar text="Search Projects" showFilters={this.props.showFilters} filterButton={true} onSearch={this.onSearch}/>
-                <ProjectsScreen/>
+                <ProjectsScreen projects={this.state.displayProjects} loading={this.state.loading}/>
             </div>
         );
-  }
+    }
 }
 
 export default Projects;
