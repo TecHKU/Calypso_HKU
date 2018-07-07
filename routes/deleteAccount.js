6ee96a1cb938dc89807598cdff6f65ba97dc7b2f
Sudhakar Shah <sudhakar.shah@tinklabs.com>
2018-07-07 19:50:28 +0800

    delete account api added

--- routes/deleteAccount.js
+++ routes/deleteAccount.js
@@ -1,26 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var Account = require('../models/account');
+var Project= require('../models/project');
 
-let standardResponse = {
-    success: false,
-    reason: "",
-    account: ""
-};
 
 router.post('/',function(req,res) {
+  let standardResponse = {
+      success: false,
+      reason: "",
+      account: ""
+  };
   Account.findByIdAndRemove(req.session.user._id,function(error, account) {
     if (error) {
       standardResponse.reason = "DbError " + error;
+      return res.send(standardResponse);
     }
     else if (!account) {
       standardResponse.reason = "Account with the id does not exixts";
+      return res.send(standardResponse);
     }
     else {
       standardResponse.success = true;
       standardResponse.account = account;
+      Project.deleteMany({author:req.session.user._id}, function(err,obj){
+        if(error){
+          standardResponse.reason = "Account deleted but projects could not be deleted";
+        }
+        return res.send(standardResponse);     
+      })
     }
-    return res.send(standardResponse);
+
   })
 })
 
