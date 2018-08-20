86f9c9e6f34c6f0808d5ad70342270a9e863197b
Sudhakar Shah <sudhakarshah2013@gmail.com>
2018-08-20 14:16:32 +0530

    env variables added

--- routes/sendEmail.js
+++ routes/sendEmail.js
@@ -1,24 +1,27 @@
-const emailConfig = require('./email-config')();
+// const emailConfig = require('./email-config')();
+const emailConfig = {
+  apiKey: process.env.EMAIL_API_KEY,
+  domain: process.env.EMAIL_DOMAIN
+};
 const mailgun = require('mailgun-js')(emailConfig);
 
 const sendEmail = (recipient, message, attachment) =>
-  new Promise((resolve, reject) => {
-    const data = {
-      from: 'Calypso <postmaster@sandbox6e3231a8b9b441419774a8fb264f98c8.mailgun.org>',
-      to: recipient,
-      subject: message.subject,
-      text: message.text,
-      inline: attachment,
-      html: message.html,
-    };
-    mailgun.messages().send(data, (error) => {
-      if (error) {
-		console.log('error in mail');
-        return reject(error);
-      }
-	  console.log('sent and resolving');
-      return resolve();
-    });
-  });
-  
+	new Promise((resolve, reject) => {
+		const data = {
+			from: `Calypso <postmaster@${emailConfig.domain}>`,
+			to: recipient,
+			subject: message.subject,
+			text: message.text,
+			inline: attachment,
+			html: message.html,
+		};
+		mailgun.messages().send(data, (error) => {
+			if (error) {
+				console.log('error in mail');
+				return reject(error);
+			}
+			console.log('sent and resolving');
+			return resolve();
+		});
+	});
 module.exports = sendEmail;
