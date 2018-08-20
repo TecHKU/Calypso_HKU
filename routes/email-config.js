24d668ca490b6d0154eab5ec9819e7263ad786b4
Sudhakar Shah <sudhakarshah2013@gmail.com>
2018-08-20 18:10:17 +0530

    config file removed

--- routes/email-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = () => {
-  const emailConfig = {
-    apiKey: '6840c720a634b6e10a20a444ebc4987c-7efe8d73-46fb188a',
-    domain: 'sandbox6e3231a8b9b441419774a8fb264f98c8.mailgun.org'
-  };
-return emailConfig;
-};
